15a2c7f9b7445e68b33fa9cd090218d499e5d26c
agalardo <agallardo@yanedx.ru>
2015-09-20 19:16:04 +0300

    change regular expression

--- application/scripts/google_regexp.php
+++ application/scripts/google_regexp.php
@@ -1,5 +1,5 @@
 <?php
-preg_match_all('/<h3 class=["]?r["]?><a href="http:\/\/([^>]+?)"[^>]+?>/', $response, $matches);  //URL of site
+preg_match_all('/<h3 class=["]?r["]?><a href="(?:http|https):\/\/([^>]+?)"[^>]+?>/', $response, $matches);  //URL of site 20.09.2015
 preg_match_all('/<td><a class=["]?fl["]? href="([^>]+?)">/', $response, $matches_nav);  //URL of page
 preg_match_all('/<td><a class=["]?fl["]? href="[^>]+?"><span [^>]+?><\/span>(\d{1,2})<\/a><\/td>/', $response, $page_nav);  //Number of page
 ?>
--- application/scripts/yandex_regexp.php
+++ application/scripts/yandex_regexp.php
@@ -11,39 +11,39 @@ if(count($page_nav[1]) < count($matches_nav[1])){
 //-----------------------------------------------
 */
 //12.08.2015
-preg_match_all('/<a class="link serp-url__link" target="_blank" href="http:\/\/([^>]+?)"[^>]+?>/', $response, $matches); //URL of site 19.04.2015
+preg_match_all('/<a class="link link_cropped_no serp-item__title-link" target="_blank" href="(?:http|https):\/\/([^>]+?)"[^>]+?>/', $response, $matches); //URL of site 20.09.2015
 preg_match_all('/<a class="button button_theme_pseudo button_counter_yes button_pseudo_yes button_size_s link link_pseudo_yes i-bem" [^>]+? href="([^>]+?)"[^>]+?>/', $response, $matches_nav);  //URL of page 24.04.2015
 preg_match_all('/<a class="button button_theme_pseudo button_counter_yes button_pseudo_yes button_size_s link link_pseudo_yes i-bem" [^>]+? href="[^>]+?"[^>]+?><span class="button__text">(\d{1,3})<\/span><\/a>/', $response, $page_nav);  //Number of page 24.04.2015
 
 
 
 //Убираем из массива рекламные ссылки и дубли искомых сайтов--------------------
-$temp = array();
-foreach ($matches[1] as $key => $value) {
-    if(strpos($value, 'yabs.yandex.ru') === false) $temp[] = $value;
-}
-$matches[1] = $temp;
-unset($temp);
-
-$keyArr = array();
-foreach ($matches[1] as $key => $value) {
-    
-    if(in_array($key, $keyArr)) continue;
-    
-    foreach ($matches[1] as $key2 => $value2) {
-       if(strpos($value2, $value) !== false){
-           $temp0[] = $value;
-           $keyArr[] = $key2;
-       }
-    }
-    $temp[] = array_pop($temp0);
-    unset($temp0);
-}
-
-
-$matches[1] = $temp;
-unset($temp);
-unset($keyArr);
+//$temp = array();
+//foreach ($matches[1] as $key => $value) {
+//    if(strpos($value, 'yabs.yandex.ru') === false) $temp[] = $value;
+//}
+//$matches[1] = $temp;
+//unset($temp);
+//
+//$keyArr = array();
+//foreach ($matches[1] as $key => $value) {
+//    
+//    if(in_array($key, $keyArr)) continue;
+//    
+//    foreach ($matches[1] as $key2 => $value2) {
+//       if(strpos($value2, $value) !== false){
+//           $temp0[] = $value;
+//           $keyArr[] = $key2;
+//       }
+//    }
+//    $temp[] = array_pop($temp0);
+//    unset($temp0);
+//}
+//
+//
+//$matches[1] = $temp;
+//unset($temp);
+//unset($keyArr);
 //------------------------------------------------------------------------------
 
 
